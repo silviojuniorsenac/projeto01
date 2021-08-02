fd05ac61e61cd8054942dc160051dee188f30197
silvio.junior <silvio.junior@PLE111D31.alunos.sc.senac.br>
2021-08-02 19:28:42 -0300

    Projeto da faculdade aprendendo lógica com POO

---
+++ src/util/GeradorUtil.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author silvio.junior
+ */
+public class GeradorUtil {
+  
+    public double gerarSalario(){
+        int numero = (int) (Math.random() * 100000);
+        return numero;
+    }
+    
+    public static void main(String[] args) {
+       GeradorUtil objetoDessaClasse = new GeradorUtil();
+        System.out.println("Salário: " + objetoDessaClasse.gerarSalario());
+    }
+}
